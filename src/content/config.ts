2cc271b8fc109b4f0e0910e456425a90b97eaad9
Arvi Acuzar <arvi.acuzar@gmail.com>
2023-09-30 16:41:25 +0800

    feat: add content collection, add sample posts, add validation to front matter using zod

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content'
+import { format } from 'date-fns'
+
+// define your collection(s)
+const postsCollection = defineCollection({
+  schema: z.object({
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z.string().transform((str) => format(new Date(str), 'MMM d, yyyy')),
+    featured: z.boolean().default(false),
+    image: z.string(),
+    title: z.string(),
+  }),
+})
+
+// export a single `collections` object to register your collection(s).
+// this key should match your collection directory name in src/content
+export const collections = {
+  posts: postsCollection,
+}
